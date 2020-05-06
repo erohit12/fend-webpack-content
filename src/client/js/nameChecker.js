725a0023823c21054713b825fd2034a7de723700
Rohit Garg <garg.rohit.robotics@gmail.com>
2020-05-06 13:44:26 +0530

    Adding babel

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,4 +1,4 @@
-function checkForName(inputText) {
+export function checkForName(inputText) {
     console.log("::: Running checkForName :::", inputText);
     let names = [
         "Picard",
@@ -12,3 +12,4 @@ function checkForName(inputText) {
         alert("Welcome, Captain!")
     }
 }
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './src/client/index.js',
+    module: 
+    {
+        rules:
+        [
+            {
+                test: '/\.js$',
+                exclude: /node_modules/,
+                loader: "babel-loader"                
+            }
+        ] 
+        
+    }
     
 }
